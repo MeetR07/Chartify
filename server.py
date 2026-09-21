b1edaba499c57333e28f82830c3464d5a39e3d0f
Meet <meetramani124421@gmail.com>
2026-09-21 17:35:20 +0530

    Bug fix

--- server.py
+++ server.py
@@ -643,5 +643,7 @@ def export_directly_to_user_downloads(req: ExportRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-
-
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port, reload=False)
